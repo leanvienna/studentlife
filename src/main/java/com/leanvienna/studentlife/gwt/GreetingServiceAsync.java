79de3ebc56531d4e3f75cfef3623e97f9bf71050
Florian Motlik <flomotlik@gmail.com>
2010-07-04 18:46:58 +0200

    for now
    Signed-off-by: Florian Motlik <flomotlik@gmail.com>

---
+++ src/main/java/com/leanvienna/studentlife/gwt/GreetingServiceAsync.java
@@ -0,0 +1,11 @@
+package com.leanvienna.studentlife.gwt;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+/**
+ * The async counterpart of <code>GreetingService</code>.
+ */
+public interface GreetingServiceAsync {
+	void greetServer(String input, AsyncCallback<String> callback)
+			throws IllegalArgumentException;
+}
