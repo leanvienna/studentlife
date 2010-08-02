603f5dfca224452692cacdf01033b06f1fb92eae
Florian Motlik <flomotlik@gmail.com>
2010-08-02 23:32:59 +0200

    Removing greeting service
    Signed-off-by: Florian Motlik <flomotlik@gmail.com>

--- src/com/leanvienna/studentlife/client/Studentlife_gwt.java
+++ src/com/leanvienna/studentlife/client/Studentlife_gwt.java
@@ -1,7 +1,6 @@
 package com.leanvienna.studentlife.client;
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.leanvienna.studentlife.client.composites.Tabs;
 
@@ -9,12 +8,6 @@ import com.leanvienna.studentlife.client.composites.Tabs;
  * Entry point classes define <code>onModuleLoad()</code>.
  */
 public class Studentlife_gwt implements EntryPoint {
-
-	/**
-	 * Create a remote service proxy to talk to the server-side Greeting service.
-	 */
-	private final GreetingServiceAsync greetingService = GWT
-			.create(GreetingService.class);
 
 	/**
 	 * This is the entry point method.
